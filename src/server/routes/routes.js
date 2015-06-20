e30eb3dfb9328286800b0e121ed23360a0537250
kenjones21 <cubsfan@udel.edu>
2015-06-20 16:40:20 -0400

    Whats happening

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -26,7 +26,7 @@ router.post('/mail', function(req, res) {
   var form = new multiparty.Form();
 
   form.parse(req, function(err, fields, files) {
-    console.log(Object.keys(fields.mailinMsg.0));
+    console.log(fields.mailinMsg.0);
     res.writeHead(200, {'content-type': 'text/plain'});
     res.write('received upload:\n\n');
     res.end(util.inspect({fields: fields, files: files}));
