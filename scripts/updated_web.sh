3ee1f5e018d5ccfc2f7484690d481471f9aba0a5
Ken Weaver <kbweaver221@gmail.com>
2020-11-06 13:56:07 -0500

    Fresh start

--- scripts/updated_web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Updating kenweaver.me"
-
-ssh kbweaver@kenweaver.me "cd kenWeaver/; git pull; exit"
--- src/server/models/PageView.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var pageViewSchema = new Schema({
-  url: String,
-  count: {type: Number, default: 0}
-});
-
-module.exports = mongoose.model('PageView', pageViewSchema);
